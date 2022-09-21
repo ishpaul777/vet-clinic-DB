7da586ef33c27ee6e294fc6325286a9c2113f27a
Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>
2022-09-21 13:46:58 +0530

    Added new data of animals

--- data.sql
+++ data.sql
@@ -15,17 +15,19 @@
  INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
  VALUES ('Devimon', '12-May-2017', 5, TRUE, 11);
  
+ 
+ /*Adding new data*/
  INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
- VALUES ('Charmander', '08-Feb-2020', FALSE, 0, 11);
+ VALUES ('Charmander', '08-Feb-2020', FALSE, 0, -11);
  
  INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
- VALUES ('Plantmon', '15-Nov-2021', 2, TRUE, 5.7);
+ VALUES ('Plantmon', '15-Nov-2021', 2, TRUE, -5.7);
  
  INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
- VALUES ('Squirtle', '02-Apr-1993', 3, FALSE, 12.13);
+ VALUES ('Squirtle', '02-Apr-1993', 3, FALSE, -12.13);
  
  INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
- VALUES ('Angemon', '12-Jul-2005', 1, TRUE, 45);
+ VALUES ('Angemon', '12-Jul-2005', 1, TRUE, -45);
  
  INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
  VALUES ('Boarmon', '7-Jun-2005', , TRUE,, 20.4);
