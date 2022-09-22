c12489e193a127d9fc9ee9a810e4bb7022c43435
Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>
2022-09-22 15:37:15 +0530

    added owner id as foreign key

--- schema.sql
+++ schema.sql
@@ -34,6 +34,7 @@ ALTER TABLE animals DROP COLUMN species;
 
 /*Add column species_id which is a foreign key referencing species table*/
 ALTER TABLE animals ADD COLUMN species_id INT;
-
-/*Add column owner_id which is a foreign key referencing the owners table*/
 ALTER TABLE animals ADD CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCES species (id)
+/*Add column owner_id which is a foreign key referencing the owners table*/
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT owner_fk FOREIGN KEY (owner_id) REFERENCES owner (id)
